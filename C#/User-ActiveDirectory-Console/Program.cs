7f09c6e7666908bd3834bcf03885217f76f32b87
AnSt <starinin-andrey@ya.ru>
2017-11-29 14:43:46 +0300

    Инициализация

---
+++ C#/User-ActiveDirectory-Console/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using User_ActiveDirectory_lib;
+
+namespace User_ActiveDirectory_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            UserAD userAD = new UserAD();
+
+            name = Console.ReadLine();
+
+            userAD.Find(name);
+
+            Console.WriteLine(userAD.SearchResultCollection.ToString());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#/User-ActiveDirectory-lib/UserAD.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.DirectoryServices;
+
+namespace User_ActiveDirectory_lib
+{
+    public class UserAD
+    {
+        //private const string username = "username";
+        //private const string password = "password";
+        private readonly string path;
+        private DirectoryEntry domain;
+        private DirectorySearcher search;
+        private SearchResultCollection searchResultCollection;
+        private SearchResult searchResult;
+
+        public UserAD()
+        {
+            domain = new DirectoryEntry();
+            search = new DirectorySearcher(domain);
+        }
+
+        public UserAD(string _path)
+        {
+            path = _path ?? throw new ArgumentNullException(nameof(_path));
+
+            domain = new DirectoryEntry(path: path);
+            search = new DirectorySearcher(domain);
+        }
+
+        public DirectoryEntry Domain { get; }//Возвращает значение переменной domain
+
+        public DirectorySearcher Search { get; }//Возвращает значение переменной search
+
+        public SearchResultCollection SearchResultCollection { get => searchResultCollection; set => searchResultCollection = value; }
+        public SearchResult SearchResult { get => searchResult; set => searchResult = value; }
+
+        public void Find(string _name)
+        {
+            search.Filter = "(&(&(objectClass=user)(objectClass=person))(cn={_name}))";
+            search.PageSize = 1000;
+            searchResultCollection = search.FindAll();
+        }
+    }
+}
